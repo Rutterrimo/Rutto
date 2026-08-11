f735571fd67c50649dce4979f10b01da77bfeaff
Alessia Fassoli <fassoli1605@gmail.com>
2026-08-11 10:38:52 +0200

    Configure map options for world boundaries

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const map = L.map('map-container').setView([20, 0], 2);
+const map = L.map('map-container', {
+    worldCopyJump: false,
+    maxBounds: [
+        [-85, -180],
+        [85, 180]
+    ],
+    maxBoundsViscosity: 1.0
+}).setView([20, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    noWrap: true,
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
